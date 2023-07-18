022e286ed0950f73ebc4c5dd772f6b5d4d731717
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-07-18 18:01:16 -0300

    brushing up from 0

---
+++ practica1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nombre="Alan";
+    $apellido="Lasco";
+    ?>
+    <h6><?= "$nombre $apellido" ?></h6>
+
+</body>
+</html>
